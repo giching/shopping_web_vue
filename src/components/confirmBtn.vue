12dfa036b50b8c042fad66c681440c4c064b8580
Giching <wendy57372@gmail.com>
2024-03-04 11:17:30 +0800

    btn components

---
+++ src/components/confirmBtn.vue
@@ -0,0 +1,17 @@
+<!-- 確定按鈕 -->
+<script>
+
+</script>
+
+<template>
+   
+        <p>確定</p>
+    
+
+</template>
+
+<style scoped>
+    p{
+        @apply bg-slate-700 text-white rounded p-2 text-center w-[100px];
+    }
+</style>
---
+++ src/components/goBackPrev.vue
@@ -0,0 +1,17 @@
+<!-- 回前一頁 -->
+<script>
+
+</script>
+
+<template>
+        <p>回前一頁</p>
+    
+
+</template>
+
+<style scoped>
+
+    p{
+        @apply bg-slate-700 text-white rounded p-2 text-center w-[100px];
+    }
+</style>
